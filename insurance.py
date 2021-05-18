806064420daa51f06abf1acd3b729fba4e3fdac3
NAMAN SINGH <19620452+naman5@users.noreply.github.com>
2021-05-18 14:25:31 +0530

    Added a smoker analyzer and bmi analyzer
    This new feature will analyze the cost based on two parameter smoker and bmi. Then it will give advice on how to reduce the insurance cost based on these two parameters.

--- insurance.py
+++ insurance.py
@@ -1,8 +1,28 @@
-# Insurance-cost-calculator
+#analyze smoker function 
+def analyze_smoker(smoker_status):
+  if smoker_status == 1:
+    print("To lower your cost, you should consider quitting smoking.")
+  else:
+    print("Smoking is not an issue for you.")
+# Analyze bmi function
+def analyze_bmi(bmi_value):
+  if bmi_value > 30:
+    print("Your BMI is in the obese range. To lower your cost, you should significantly lower your BMI.")
+  elif bmi_value >=25 and bmi_value <= 30:
+    print("Your BMI is in the overweight range. To lower your cost, you should lower your BMI.")
+  elif bmi_value >= 18.5 and bmi_value < 25:
+    print("Your BMI is in a healthy range.")
+  elif bmi < 18.5:
+    print("Your BMI is in the underweight range. Increasing your BMI will not help lower your cost, but it will help improve your health.")
+  else:
+    print("Erro!!")
+
 # Create calculate_insurance_cost() function below: 
 def calculate_insurance_cost(age, sex, bmi, num_of_children, smoker, name):
   estimated_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
   print ("The estimated insurance cost for" + name + "is " + str(estimated_cost) + " dollars.")
+  analyze_smoker(smoker)
+  analyze_bmi(bmi)
   return estimated_cost
 
 # Estimate Maria's insurance cost
